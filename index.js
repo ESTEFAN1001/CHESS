54418327284becedb918f47ce3a0416ab8c80af8
Dorian <ccd0029570@est.univalle.edu>
2026-06-30 13:04:18 -0400

    refix: priority IPv4

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 require('dotenv').config();
+
+// ipv4
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const path = require('path');
 require(path.join(__dirname, 'server', 'server.js'));
